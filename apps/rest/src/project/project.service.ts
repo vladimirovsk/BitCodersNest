fad7dc455052874e475ceddc6e2eb4acdc6272fd
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-01-22 07:52:03 -0700

    fix auth

--- apps/rest/src/project/project.service.ts
+++ apps/rest/src/project/project.service.ts
@@ -3,14 +3,15 @@ import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectModel } from './project.model';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from 'typegoose';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ProjectService {
   constructor(
+    private readonly configService: ConfigService,
     @InjectModel(ProjectModel)
     private projectModel: ModelType<ProjectModel>,
-  ) {
-  }
+  ) {}
 
   async create(createUserDto: CreateProjectDto): Promise<ProjectModel> {
     const createProject = new this.projectModel(createUserDto);
@@ -21,7 +22,20 @@ export class ProjectService {
     return await this.projectModel.findById(id).exec();
   }
 
-  async findAll(): Promise<ProjectModel[]> {
-    return await this.projectModel.find().exec();
+  async findAll() {
+    //Promise<ProjectModel[]>
+    const strConfig = `mongodb://${this.configService.get(
+      'MONGO_USER',
+    )}:${this.configService.get('MONGO_PASSWORD')}@${this.configService.get(
+      'MONGO_HOST',
+    )}:${this.configService.get('MONGO_PORT')}/${this.configService.get(
+      'MONGO_DB',
+    )}`;
+
+    const result = await this.projectModel.find().exec();
+    return {
+      strConfig,
+      result,
+    };
   }
 }
