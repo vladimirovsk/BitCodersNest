bc4825585a9817f93b4cb88792b03f9cf12ea779
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-12-23 19:11:11 -0700

    config port checker

--- apps/services/src/telegram-bot/telegram-bot.service.ts
+++ apps/services/src/telegram-bot/telegram-bot.service.ts
@@ -1,60 +1,66 @@
-import { forwardRef, Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
 import { Context, deunionize, Telegraf } from 'telegraf';
 import {
-	Ctx,
-	Hears,
-	Help, InjectBot,
-	Action,
-	On,
-	Start,
-	Update,
-	Command
+  Command,
+  Ctx,
+  Help,
+  InjectBot,
+  On,
+  Start,
+  Update,
 } from 'nestjs-telegraf';
 import { PortCheckerService } from '../portChecker/port-checker.service';
+import { ConfigService } from '@nestjs/config';
 
 @Update()
 @Injectable()
-export class TelegramBotService  {
-	private logger = new Logger(TelegramBotService.name);
-	private chatId = '-4040356453';
+export class TelegramBotService {
+  private logger = new Logger(TelegramBotService.name);
+  private chatId = <string>this.configService.get('TELEGRAM_BOT_CHANEL');
 
-	constructor(
-		@InjectBot() private bot: Telegraf<Context>,
-		@Inject(forwardRef(() => PortCheckerService))
-		private portCheckerService: PortCheckerService,
-	) {
-	}
-	async sendMessage(message: string, chatId:string ){
-		this.bot.telegram.sendMessage(chatId, message).catch(err=>{
-		this.logger.debug(chatId, message, err.message);
-		throw new Error(err.message);
-		});
-	}
+  constructor(
+    @InjectBot() private bot: Telegraf<Context>,
+    @Inject(forwardRef(() => PortCheckerService))
+    private portCheckerService: PortCheckerService,
+    private configService: ConfigService,
+  ) {}
 
-	@Command( 'status')
-	async status(ctx: Context) {
-		await this.portCheckerService.getStatusServersDto(true)
-			.catch((e) => this.logger.error(`Error get status servers ${e.message}`))
-		this.logger.debug('Send status')
-	}
+  async sendMessage(message: string, chatId: string) {
+    this.bot.telegram.sendMessage(chatId, message).catch((err) => {
+      this.logger.error(chatId, message, err.message);
+      throw new Error(err.message);
+    });
+  }
 
-	@Help()
-	async helpCommand(ctx: Context) {
-		await ctx.reply('List my commands....');
-		this.logger.debug('Send List my commands')
-	}
+  @Command('status')
+  async status(ctx: Context) {
+    await this.portCheckerService
+      .getStatusServersDto(true)
+      .then((response) => {
+        if (response) {
+          this.sendMessage(JSON.stringify(response, null, 2), this.chatId);
+        }
+      })
+      .catch((e) => this.logger.error(`Error get status servers ${e.message}`));
+    this.logger.debug('Send status');
+  }
 
-	@Start()
-	async start(@Ctx() ctx : Context){
-		await ctx.reply(`Hi! i'm telegram bot company Bitcoders.`);
-		this.logger.debug('Send start message')
-	}
+  @Help()
+  async helpCommand(ctx: Context) {
+    await ctx.reply('/status \n /help');
+    this.logger.debug('Send List my commands');
+  }
 
-	@On('text')
-	async hearsAll(ctx: Context) {
-		const text = deunionize(ctx.message)?.text;
-		await ctx.reply(`YOU WRITE ME ${JSON.stringify(text, null, 2)}`);
-		this.logger.debug('Reply text message')
-	}
+  @Start()
+  async start(@Ctx() ctx: Context) {
+    await ctx.reply(`Hi! i'm telegram bot company Bitcoders.`);
+    this.logger.debug('Send start message');
+  }
 
+  @On('text')
+  async hearsAll(ctx: Context) {
+    const text = deunionize(ctx.message)?.text;
+    await ctx.reply(`YOU WRITE ME ${JSON.stringify(text, null, 2)}`);
+    this.logger.debug('Reply text message');
+  }
 }
